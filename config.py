c4c595b6d435efbdeaed851b228a9fb5af5c8890
Quinton Pang Yi Xuan <87934498+QuintonPang@users.noreply.github.com>
2025-08-26 15:26:10 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ccgitlvjiezp.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "quintonpyx-employee2"
+customregion = "us-east-1"
+
